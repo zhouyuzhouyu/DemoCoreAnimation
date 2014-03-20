a2aa1f6c00e126e7c1b58f4ef901c8f532fed61b
zhouyu <524112470@qq.com>
2014-03-20 15:16:26 +0800

    init

---
+++ DemoCoreAnimation/DemoCoreAnimation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoCoreAnimation
+//
+//  Created by Allen1 on 14-3-20.
+//  Copyright (c) 2014年 zhouyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ DemoCoreAnimation/DemoCoreAnimationTests/DemoCoreAnimationTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoCoreAnimationTests.m
+//  DemoCoreAnimationTests
+//
+//  Created by Allen1 on 14-3-20.
+//  Copyright (c) 2014年 zhouyu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoCoreAnimationTests : XCTestCase
+
+@end
+
+@implementation DemoCoreAnimationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
